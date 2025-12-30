edd796c52779b7566029932f6cfaa406f665e44e
davenieur <peralesrodriguezdavid@gmail.com>
2025-12-30 00:43:30 -0600

    Agregando index.html

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "google_storage_bucket_iam_member" "public_access" {
 
 terraform {
   backend "gcs" {
-    bucket  = "cv-project-dpr-30122025" # El nombre del bucket que acabas de crear
+    bucket  = var.bucket_name # El nombre del bucket que acabas de crear
     prefix  = "terraform/state"
   }
 }
